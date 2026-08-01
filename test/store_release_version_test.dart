fc98a937ee96f6f1e477eda8672400315a08f6f0
Methee Treewichian <mt.koo.dev@gmail.com>
2026-08-01 13:11:59 +0700

    release: bump Sookta to 1.3.8+25

--- test/store_release_version_test.dart
+++ test/store_release_version_test.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('store release uses the new 1.3.7 train and synchronized build 24', () {
+  test('store release uses the new 1.3.8 train and synchronized build 25', () {
     final pubspec = File('pubspec.yaml').readAsStringSync();
     final buildInfo = File('lib/app/build_info.dart').readAsStringSync();
 
-    expect(pubspec, contains('version: 1.3.7+24'));
-    expect(buildInfo, contains("versionName = '1.3.7'"));
-    expect(buildInfo, contains("buildNumber = '24'"));
+    expect(pubspec, contains('version: 1.3.8+25'));
+    expect(buildInfo, contains("versionName = '1.3.8'"));
+    expect(buildInfo, contains("buildNumber = '25'"));
   });
 }
